19001c4a4bb1f7bced1d2362dfbcae300484117e
Mani <mcnc33@gmail.com>
2024-09-29 13:02:32 -0400

    next cofig export fix

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    reactStrictMode: true,
-    env: {
-      CLERK_SECRET_KEY: process.env.CLERK_SECRET_KEY,
-      NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY: process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY,
-    },
-  }
-  
-  module.exports = nextConfig
+  reactStrictMode: true,
+  env: {
+    CLERK_SECRET_KEY: process.env.CLERK_SECRET_KEY,
+    NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY: process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY,
+  },
+}
+
+export default nextConfig;
