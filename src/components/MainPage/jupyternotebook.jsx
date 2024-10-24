74857fecabf971440438d6128c63187173d4a7fa
allenki_akshay <allenkiakshay8322@gmail.com>
2024-10-24 09:28:09 +0530

    Refactor loading and launch functionality in vscode and jupyternotebook components

--- src/components/MainPage/jupyternotebook.jsx
+++ src/components/MainPage/jupyternotebook.jsx
@@ -59,14 +59,16 @@ const JupyterNotebook = () => {
       // const newTab = window.open("", "_blank");
       // newTab.location.href = url;
       setUrl(url);
+      const timestamp = Date.now();
+      localStorage.setItem("vscInstanceTimestamp", timestamp);
     } else {
       console.error("Failed to retrieve URL");
     }
 
     setTimeout(async () => {
       setLoading(false);
-      setUrl(null);
-    }, 20000);
+      // setUrl(null);
+    }, 900000);
   };
 
   useEffect(() => {
@@ -78,6 +80,23 @@ const JupyterNotebook = () => {
       setOrgName("testing");
     } else {
       setOrgName(subdomain);
+    }
+  }, []);
+
+  useEffect(() => {
+    const timestamp = localStorage.getItem("vscInstanceTimestamp");
+    const currentTime = Date.now();
+    const timeDiff = currentTime - timestamp;
+
+    if (timeDiff < 900000) {
+      let storedVSCUrl = Cookies.get("storedVSCUrl");
+      storedVSCUrl = storedVSCUrl.split("/auth")[0];
+      storedVSCUrl = storedVSCUrl + "/codeeditor/default";
+      setLoading(true);
+      setUrl(storedVSCUrl);
+    } else {
+      localStorage.removeItem("vscInstanceTimestamp");
+      setUrl(null);
     }
   }, []);
 
