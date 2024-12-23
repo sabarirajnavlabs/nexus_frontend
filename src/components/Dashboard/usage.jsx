904add90d13c99b4f51436d48dbdb4b7e1cff7ea
allenki_akshay <allenkiakshay8322@gmail.com>
2024-12-23 20:43:51 +0530

    Refactor loading and launch functionality in ChatPage, UsageReport, and Sidebar components

--- src/components/Dashboard/usage.jsx
+++ src/components/Dashboard/usage.jsx
@@ -2,27 +2,6 @@
 import { useState, useEffect } from "react";
 import { useTheme } from "next-themes";
 import Cookies from "js-cookie";
-import { Line } from "react-chartjs-2";
-import {
-  Chart as ChartJS,
-  CategoryScale,
-  LinearScale,
-  PointElement,
-  LineElement,
-  Title,
-  Tooltip,
-  Legend,
-} from "chart.js";
-
-ChartJS.register(
-  CategoryScale,
-  LinearScale,
-  PointElement,
-  LineElement,
-  Title,
-  Tooltip,
-  Legend
-);
 
 const UsageReport = () => {
   const { theme } = useTheme();
@@ -69,50 +48,6 @@ const UsageReport = () => {
     return Math.floor((end - start) / (1000 * 60)); // return in minutes
   }
 
-  const aggregatedData = data.reduce((acc, item) => {
-    const dateKey = new Date(item.start_time).toISOString().slice(0, 10);
-    const sessionDuration = calculateActiveTime(
-      new Date(item.start_time).getTime(),
-      new Date(item.end_time).getTime()
-    );
-    if (acc[dateKey]) {
-      acc[dateKey] += sessionDuration;
-    } else {
-      acc[dateKey] = sessionDuration;
-    }
-    return acc;
-  }, {});
-
-  const chartData = {
-    labels: Object.keys(aggregatedData),
-    datasets: [
-      {
-        label: "Total Utilization Time (minutes)",
-        data: Object.values(aggregatedData),
-        borderColor: "rgb(53, 162, 235)",
-        backgroundColor: "rgba(53, 162, 235, 0.5)",
-      },
-    ],
-  };
-
-  const options = {
-    scales: {
-      y: {
-        beginAtZero: true,
-      },
-    },
-    responsive: true,
-    plugins: {
-      legend: {
-        position: "top",
-      },
-      title: {
-        display: true,
-        text: "Daily Utilization Time",
-      },
-    },
-  };
-
   return (
     <div
       className={`flex flex-col justify-center items-center p-4 ${
@@ -123,7 +58,6 @@ const UsageReport = () => {
         <div>Loading...</div>
       ) : (
         <>
-          <Line data={chartData} options={options} />
           <table
             className={`w-full text-sm ${
               theme === "dark" ? "text-white" : "text-gray-900"
