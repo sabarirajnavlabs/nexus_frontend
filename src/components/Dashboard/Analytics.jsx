0eb895e6982b71c3ebe3a891fcb959673ea8d490
allenki_akshay <allenkiakshay8322@gmail.com>
2024-12-24 13:24:40 +0530

    Refactor loading and launch functionality in Analytics component

--- src/components/Dashboard/Analytics.jsx
+++ src/components/Dashboard/Analytics.jsx
@@ -1,14 +1,222 @@
+// "use client";
+// import { useState, useEffect } from "react";
+// import { useTheme } from "next-themes";
+// import Cookies from "js-cookie";
+// import { Line } from "react-chartjs-2";
+// import {
+//   Chart as ChartJS,
+//   CategoryScale,
+//   LinearScale,
+//   PointElement,
+//   LineElement,
+//   Title,
+//   Tooltip,
+//   Legend,
+// } from "chart.js";
+
+// ChartJS.register(
+//   CategoryScale,
+//   LinearScale,
+//   PointElement,
+//   LineElement,
+//   Title,
+//   Tooltip,
+//   Legend
+// );
+
+// const UsageReport = () => {
+//   const { theme } = useTheme();
+//   const [data, setData] = useState([]);
+//   const [loading, setLoading] = useState(true);
+//   const [orgName, setOrgName] = useState("testing");
+//   const endpoint = process.env.NEXT_PUBLIC_API_URL;
+
+//   useEffect(() => {
+//     const fetchData = async () => {
+//       setLoading(true);
+//       try {
+//         const response = await fetch(`${endpoint}/${orgName}/logs`, {
+//           method: "GET",
+//           headers: {
+//             "Content-Type": "application/json",
+//             Token: Cookies.get("__session") || "",
+//           },
+//         });
+//         const data = await response.json();
+//         setData(data);
+//         setLoading(false);
+//       } catch (error) {
+//         console.error("Failed to fetch data:", error);
+//         setLoading(false);
+//       }
+//     };
+
+//     fetchData();
+//   }, [endpoint, orgName]);
+
+//   function calculateActiveTime(start, end) {
+//     return Math.floor((end - start) / (1000 * 60)); // return in minutes
+//   }
+
+//   const getWeekStartDate = (date) => {
+//     const dayIndex = date.getDay();
+//     const startDate = new Date(date);
+//     startDate.setDate(date.getDate() - dayIndex);
+//     return startDate.toISOString().slice(0, 10);
+//   };
+
+//   const aggregateData = (data, timeFrame) => {
+//     return data.reduce((acc, item) => {
+//       const startTime = new Date(item.start_time);
+//       const endTime = new Date(item.end_time);
+//       let key;
+//       switch (timeFrame) {
+//         case "daily":
+//           key = startTime.toISOString().slice(0, 10);
+//           break;
+//         case "weekly":
+//           key = getWeekStartDate(startTime);
+//           break;
+//         case "monthly":
+//           key = `${startTime.getFullYear()}-${String(
+//             startTime.getMonth() + 1
+//           ).padStart(2, "0")}`;
+//           break;
+//       }
+//       if (!acc[key]) {
+//         acc[key] = {};
+//       }
+//       acc[key][item.app_type] =
+//         (acc[key][item.app_type] || 0) +
+//         calculateActiveTime(startTime.getTime(), endTime.getTime());
+//       return acc;
+//     }, {});
+//   };
+
+//   const dailyData = aggregateData(data, "daily");
+//   const weeklyData = aggregateData(data, "weekly");
+//   const monthlyData = aggregateData(data, "monthly");
+
+//   const createChartData = (data, label, isSplit = false) => {
+//     if (!isSplit) {
+//       return {
+//         labels: Object.keys(data),
+//         datasets: [
+//           {
+//             label: label,
+//             data: Object.keys(data).map((key) =>
+//               Object.values(data[key]).reduce((a, b) => a + b, 0)
+//             ),
+//             borderColor: "rgb(53, 162, 235)",
+//             backgroundColor: "rgba(53, 162, 235, 0.5)",
+//           },
+//         ],
+//       };
+//     } else {
+//       return {
+//         labels: Object.keys(data),
+//         datasets: [
+//           {
+//             label: "Jupyter Notebook (minutes)",
+//             data: Object.keys(data).map(
+//               (key) => data[key]["JupyterLab"] || 0
+//             ),
+//             borderColor: "rgb(255, 99, 132)",
+//             backgroundColor: "rgba(255, 99, 132, 0.5)",
+//           },
+//           {
+//             label: "VSCode (minutes)",
+//             data: Object.keys(data).map((key) => data[key]["CodeEditor"] || 0),
+//             borderColor: "rgb(54, 162, 235)",
+//             backgroundColor: "rgba(54, 162, 235, 0.5)",
+//           },
+//         ],
+//       };
+//     }
+//   };
+
+//   const options = {
+//     scales: {
+//       y: {
+//         beginAtZero: true,
+//       },
+//     },
+//     responsive: true,
+//     plugins: {
+//       legend: {
+//         position: "top",
+//       },
+//       title: {
+//         display: true,
+//         text: "Utilization Time",
+//       },
+//     },
+//     maintainAspectRatio: false, // Control aspect ratio
+//   };
+
+//   return (
+//     <div
+//       className={`flex flex-col gap-28 justify-center items-center p-4 ${
+//         theme === "dark" ? "bg-[#121212]" : "bg-white"
+//       } ml-16 w-full h-full`}
+//     >
+//       {loading ? (
+//         <div>Loading...</div>
+//       ) : (
+//         <>
+//           <div style={{ width: "94.7%", height: "300px" }}>
+//             <Line
+//               data={createChartData(
+//                 dailyData,
+//                 "Daily Utilization Time (minutes)"
+//               )}
+//               options={options}
+//             />
+//           </div>
+//           <div style={{ width: "94.7%", height: "300px" }}>
+//             <Line
+//               data={createChartData(
+//                 weeklyData,
+//                 "Weekly Utilization Time by Application",
+//                 true
+//               )}
+//               options={{
+//                 ...options,
+//                 title: {
+//                   ...options.title,
+//                   text: "Weekly Utilization Time by Application",
+//                 },
+//               }}
+//             />
+//           </div>
+//           <div style={{ width: "94.7%", height: "300px" }}>
+//             <Line
+//               data={createChartData(
+//                 monthlyData,
+//                 "Monthly Utilization Time (minutes)"
+//               )}
+//               options={options}
+//             />
+//           </div>
+//         </>
+//       )}
+//     </div>
+//   );
+// };
+
+// export default UsageReport;
+
+
 "use client";
 import { useState, useEffect } from "react";
 import { useTheme } from "next-themes";
 import Cookies from "js-cookie";
-import { Line } from "react-chartjs-2";
+import { Bar } from "react-chartjs-2";
 import {
   Chart as ChartJS,
   CategoryScale,
   LinearScale,
-  PointElement,
-  LineElement,
+  BarElement,
   Title,
   Tooltip,
   Legend,
@@ -17,8 +225,7 @@ import {
 ChartJS.register(
   CategoryScale,
   LinearScale,
-  PointElement,
-  LineElement,
+  BarElement,
   Title,
   Tooltip,
   Legend
@@ -42,8 +249,8 @@ const UsageReport = () => {
             Token: Cookies.get("__session") || "",
           },
         });
-        const data = await response.json();
-        setData(data);
+        const result = await response.json();
+        setData(result);
         setLoading(false);
       } catch (error) {
         console.error("Failed to fetch data:", error);
@@ -58,18 +265,17 @@ const UsageReport = () => {
     return Math.floor((end - start) / (1000 * 60)); // return in minutes
   }
 
-  // Helper to get the start date of the week
   const getWeekStartDate = (date) => {
-    const dayIndex = date.getDay(); // get the day of the week (0 for Sunday)
+    const dayIndex = date.getDay();
     const startDate = new Date(date);
-    startDate.setDate(date.getDate() - dayIndex); // set to the previous Sunday
-    return startDate.toISOString().slice(0, 10); // return as YYYY-MM-DD
+    startDate.setDate(date.getDate() - dayIndex);
+    return startDate.toISOString().slice(0, 10);
   };
 
-  const aggregateData = (data, timeFrame) => {
-    return data.reduce((acc, item) => {
-      const startTime = new Date(item.start_time);
-      const endTime = new Date(item.end_time);
+  const aggregateData = (logs, timeFrame) => {
+    return logs.reduce((acc, log) => {
+      const startTime = new Date(log.start_time);
+      const endTime = new Date(log.end_time);
       let key;
       switch (timeFrame) {
         case "daily":
@@ -79,18 +285,17 @@ const UsageReport = () => {
           key = getWeekStartDate(startTime);
           break;
         case "monthly":
-          key = `${startTime.getFullYear()}-${startTime.getMonth() + 1}`;
+          key = `${startTime.getFullYear()}-${String(
+            startTime.getMonth() + 1
+          ).padStart(2, "0")}`;
           break;
       }
-      const sessionDuration = calculateActiveTime(
-        startTime.getTime(),
-        endTime.getTime()
-      );
-      if (acc[key]) {
-        acc[key] += sessionDuration;
-      } else {
-        acc[key] = sessionDuration;
+      if (!acc[key]) {
+        acc[key] = {};
       }
+      acc[key][log.app_type] =
+        (acc[key][log.app_type] || 0) +
+        calculateActiveTime(startTime.getTime(), endTime.getTime());
       return acc;
     }, {});
   };
@@ -99,17 +304,37 @@ const UsageReport = () => {
   const weeklyData = aggregateData(data, "weekly");
   const monthlyData = aggregateData(data, "monthly");
 
-  const createChartData = (data, label) => ({
-    labels: Object.keys(data),
-    datasets: [
-      {
-        label: label,
-        data: Object.values(data),
-        borderColor: "rgb(53, 162, 235)",
-        backgroundColor: "rgba(53, 162, 235, 0.5)",
-      },
-    ],
-  });
+  const createChartData = (aggregatedData, label, isSplit = false) => {
+    if (!isSplit) {
+      return {
+        labels: Object.keys(aggregatedData),
+        datasets: [
+          {
+            label: label,
+            data: Object.keys(aggregatedData).map((key) =>
+              Object.values(aggregatedData[key]).reduce((a, b) => a + b, 0)
+            ),
+            backgroundColor: "rgba(53, 162, 235, 0.5)",
+          },
+        ],
+      };
+    } else {
+      const appTypes = Object.values(aggregatedData).flatMap(Object.keys);
+      const uniqueAppTypes = [...new Set(appTypes)];
+      return {
+        labels: Object.keys(aggregatedData),
+        datasets: uniqueAppTypes.map((appType) => ({
+          label: `${appType} (minutes)`,
+          data: Object.keys(aggregatedData).map(
+            (key) => aggregatedData[key][appType] || 0
+          ),
+          backgroundColor: `rgba(${Math.floor(Math.random() * 255)}, ${Math.floor(
+            Math.random() * 255
+          )}, ${Math.floor(Math.random() * 255)}, 0.5)`,
+        })),
+      };
+    }
+  };
 
   const options = {
     scales: {
@@ -127,7 +352,7 @@ const UsageReport = () => {
         text: "Utilization Time",
       },
     },
-    maintainAspectRatio: false, // Control aspect ratio
+    maintainAspectRatio: false,
   };
 
   return (
@@ -141,7 +366,7 @@ const UsageReport = () => {
       ) : (
         <>
           <div style={{ width: "94.7%", height: "300px" }}>
-            <Line
+            <Bar
               data={createChartData(
                 dailyData,
                 "Daily Utilization Time (minutes)"
@@ -150,16 +375,23 @@ const UsageReport = () => {
             />
           </div>
           <div style={{ width: "94.7%", height: "300px" }}>
-            <Line
+            <Bar
               data={createChartData(
                 weeklyData,
-                "Weekly Utilization Time (minutes)"
+                "Weekly Utilization Time by Application",
+                true
               )}
-              options={options}
+              options={{
+                ...options,
+                title: {
+                  ...options.title,
+                  text: "Weekly Utilization Time by Application",
+                },
+              }}
             />
           </div>
           <div style={{ width: "94.7%", height: "300px" }}>
-            <Line
+            <Bar
               data={createChartData(
                 monthlyData,
                 "Monthly Utilization Time (minutes)"
